d628fd28d52212cb9c9a1b4d250859d26b0e3d2e
ThomasMountainborn <thomasvdb90@gmail.com>
2026-09-16 13:50:58 +0200

    yield session in cleanup

--- app/services/cleanup.py
+++ app/services/cleanup.py
@@ -3,30 +3,30 @@ from datetime import datetime, UTC
 from sqlalchemy.orm import Session
 from sqlalchemy import select, or_
 from pathlib import Path
-from app.db import engine
+from app.db import get_db
 from app.models.file import UploadedFile
+from fastapi import Depends
 
-def cleanUp():
-    with Session(engine) as session:
-        stmnt = select(UploadedFile).where(
-            UploadedFile.expires_at < datetime.now(UTC)
-        )
-        
-        result = session.execute(stmnt).scalars().all()
+def cleanUp(db: Session = Depends(get_db)):
+    stmnt = select(UploadedFile).where(
+        UploadedFile.expires_at < datetime.now(UTC)
+    )
+    
+    result = db.execute(stmnt).scalars().all()
 
-        for file in result:
-            session.delete(file)
-        
-        upload_dir = Path("./data/uploads")
+    for file in result:
+        db.delete(file)
+    
+    upload_dir = Path("./data/uploads")
 
-        for file in result:
-            # complete filename on disk
-            name = file.original_filename
-            file_path = upload_dir / name
-            # no need to wrap in try block as it is safe automatically
-            file_path.unlink(missing_ok=True)
+    for file in result:
+        # complete filename on disk
+        name = file.original_filename
+        file_path = upload_dir / name
+        # no need to wrap in try block as it is safe automatically
+        file_path.unlink(missing_ok=True)
 
-        session.commit()
+    db.commit()
 
 
 scheduler = BackgroundScheduler()
