8878c2d3336a746b8e4905985947c7cd6d71e1e3
ThomasMountainborn <thomasvdb90@gmail.com>
2026-09-16 18:53:01 +0200

    Remove upload/download catch

--- app/api/files.py
+++ app/api/files.py
@@ -25,96 +25,90 @@ async def ping():
 
 @router.post("/upload")
 async def upload(request: Request, file: UploadFile = File(...)):   
+    delta = timedelta(seconds=90)
+    SIZE_LIMIT = 10*1024*1024
+    content_length = request.headers.get("content-length")
+
+    if content_length and int(content_length) > SIZE_LIMIT:
+        raise HTTPException(status_code=413, detail="File size limit exceeded.")
+
+    # check if empty file
+    if file.size == 0:
+        raise HTTPException(status_code=400, detail="Empty file not allowed.")
+    
+    # check if file is too large (compared in BYTES)
+    if file.size > SIZE_LIMIT:
+        raise HTTPException(status_code=413, detail="File size limit exceeded.")
+
+    # path to store files at
+    upload_dir = Path("./data/uploads")
+
     try:
-        delta = timedelta(seconds=90)
-        SIZE_LIMIT = 10*1024*1024
-        content_length = request.headers.get("content-length")
+        # create dir if it doesnt exist
+        upload_dir.mkdir(parents=True, exist_ok=True)
+    except OSError:
+        raise HTTPException(status_code=500, detail="Failed to prepare upload directory")
 
-        if content_length and int(content_length) > SIZE_LIMIT:
-            raise HTTPException(status_code=413, detail="File size limit exceeded.")
+    # complete file path(relative) to the file
+    destination = upload_dir / f"{file.filename}"
+    # Delete the existing file. 
+    destination.unlink(missing_ok=True)
 
-        # check if empty file
-        if file.size == 0:
-            raise HTTPException(status_code=400, detail="Empty file not allowed.")
-        
-        # check if file is too large (compared in BYTES)
-        if file.size > SIZE_LIMIT:
-            raise HTTPException(status_code=413, detail="File size limit exceeded.")
-
-        # path to store files at
-        upload_dir = Path("./data/uploads")
-
-        try:
-            # create dir if it doesnt exist
-            upload_dir.mkdir(parents=True, exist_ok=True)
-        except OSError:
-            raise HTTPException(status_code=500, detail="Failed to prepare upload directory")
-
-        # complete file path(relative) to the file
-        destination = upload_dir / f"{file.filename}"
-        # Delete the existing file. 
-        destination.unlink(missing_ok=True)
-
-        try:
-            # copy file data in chunks in bytes so huge data is not loaded into RAM
-            with destination.open("wb") as buffer:
-                shutil.copyfileobj(file.file, buffer)
-        except Exception:
-            raise HTTPException(status_code=500, detail="Failed to save file")
-
-        with Session(engine) as db:
-            uploaded_file = get_existing_record(file.filename, db)
-            createdNewRecord = False
-            if uploaded_file is None:
-                createdNewRecord = True
-                uploaded_file = UploadedFile(
-                                        original_filename=file.filename,
-                                        expires_at = datetime.now(UTC) + delta if delta else None,
-                                        )
-            else:
-                uploaded_file.expires_at = datetime.now(UTC) + delta if delta else None,
-
-            try:
-                if createdNewRecord:
-                    db.add(uploaded_file)
-                db.commit()
-            except Exception:
-                destination.unlink(missing_ok=True) # remove orphaned file on disk
-                db.rollback()
-                raise HTTPException(status_code=500, detail="Failed to commit to DB.")
-            
-            return {
-                "filename": file.filename,
-                "saved_to": str(destination),
-                "valid_till": uploaded_file.expires_at,
-                "submitted_at": datetime.now(UTC)
-            }
+    try:
+        # copy file data in chunks in bytes so huge data is not loaded into RAM
+        with destination.open("wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
     except Exception:
-        raise HTTPException(status_code=500, detail="Exception occurred during upload process.")
+        raise HTTPException(status_code=500, detail="Failed to save file")
+
+    with Session(engine) as db:
+        uploaded_file = get_existing_record(file.filename, db)
+        createdNewRecord = False
+        if uploaded_file is None:
+            createdNewRecord = True
+            uploaded_file = UploadedFile(
+                                    original_filename=file.filename,
+                                    expires_at = datetime.now(UTC) + delta if delta else None,
+                                    )
+        else:
+            uploaded_file.expires_at = datetime.now(UTC) + delta if delta else None,
+
+        try:
+            if createdNewRecord:
+                db.add(uploaded_file)
+            db.commit()
+        except Exception:
+            destination.unlink(missing_ok=True) # remove orphaned file on disk
+            db.rollback()
+            raise HTTPException(status_code=500, detail="Failed to commit to DB.")
+        
+        return {
+            "filename": file.filename,
+            "saved_to": str(destination),
+            "valid_till": uploaded_file.expires_at,
+            "submitted_at": datetime.now(UTC)
+        }
 
 @router.get("/download/{name}")
 async def download(name: str, db: Session = Depends(get_db)):
-    try:
-        statement = select(UploadedFile).where(UploadedFile.original_filename == name)
-        file_record = db.execute(statement).scalar_one_or_none()
+    statement = select(UploadedFile).where(UploadedFile.original_filename == name)
+    file_record = db.execute(statement).scalar_one_or_none()
 
-        if file_record is None:
-            raise HTTPException(status_code=404, detail="File not found")
+    if file_record is None:
+        raise HTTPException(status_code=404, detail="File not found")
 
-        if file_record.expires_at and file_record.expires_at < datetime.now(UTC):
-            raise HTTPException(status_code=404, detail="File not found")
+    if file_record.expires_at and file_record.expires_at < datetime.now(UTC):
+        raise HTTPException(status_code=404, detail="File not found")
 
-        path = Path("./data/uploads/") / f"{name}"
-        filename= file_record.original_filename
+    path = Path("./data/uploads/") / f"{name}"
+    filename= file_record.original_filename
 
-        if not path.exists():
-            db.delete(file_record)
-            db.commit()
-            raise HTTPException(status_code=404, detail="File not found")
+    if not path.exists():
+        db.delete(file_record)
+        db.commit()
+        raise HTTPException(status_code=404, detail="File not found")
 
-        return FileResponse(
-            path=path,
-            filename=filename
-        )
-    except Exception:
-        raise HTTPException(status_code=500, detail="Exception occurred during upload process.")
+    return FileResponse(
+        path=path,
+        filename=filename
+    )
