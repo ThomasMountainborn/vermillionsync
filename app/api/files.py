256117935adf7d59eb170a2007d0b5f550eb20d8
ThomasMountainborn <thomasvdb90@gmail.com>
2026-09-16 08:31:12 +0200

    Increase file ttl

--- app/api/files.py
+++ app/api/files.py
@@ -24,7 +24,7 @@ async def ping():
 
 @router.post("/upload")
 async def upload(request: Request, file: UploadFile = File(...), db: Session = Depends(get_db)):   
-    delta = timedelta(minutes=2)
+    delta = timedelta(minutes=30)
     SIZE_LIMIT = 5*1024*1024
     content_length = request.headers.get("content-length")
 
